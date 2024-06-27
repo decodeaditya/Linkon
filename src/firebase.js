31cb3658767c3fd9f117f4113f580596dcdaac2d
Aditya Pal <99229883+Impactoraditya@users.noreply.github.com>
2024-06-27 23:04:00 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyBV5O0Mwg-w-TC0wt5fUcLhNNbQTJrnm1w",
+  apiKey: "",
   authDomain: "linkon-chatapp.firebaseapp.com",
   projectId: "linkon-chatapp",
   storageBucket: "linkon-chatapp.appspot.com",
-  messagingSenderId: "4941493210",
-  appId: "1:4941493210:web:f3b833569ba3c3f9c706e3"
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
